31ce09427e5829271bfa794c63d5f6728bcb98b6
Philip Lundqvist <philiplundqvist@gmail.com>
2018-01-04 00:10:53 +0100

    Initial commit

---
+++ ITEAD-S20-CustomFW.ino
@@ -0,0 +1,303 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <EEPROM.h>
+
+//#define DEBUG // Comment/Uncomment this line to enable/disable debugging output.
+
+#define FW_VERSION "FW: v1712.02"
+
+#define PIN_RELAY 12
+#define PIN_STATUS 13
+#define PIN_BUTTON 0
+
+#define MQTT_MAX_ATTEMPT 3
+#define MQTT_CONN_TIMEOUT 60000   //Milliseconds
+#define MQTT_CONN_REATT 10000     //Milliseconds
+
+
+const char* wifiSsid = "";                  // Network SSID
+const char* wifiPass = "";                  // Network pre shared key (WPA2)
+
+const char mqttServer[] = "";               // MQTT Servers IP-address
+const int  mqttServerPort = 1883;           // MQTT Servers listening port (Default is 1883)
+const char mqttUsername[] = "";             // Leave blank if authorization is not required
+const char mqttPassword[] = "";             // Leave blank if authorization is not required
+
+
+// Program variables
+bool reportRequired = true;
+
+long lastDebounceTime = 0;
+long debounceDelay = 50;  // Milliseconds
+
+int buttonState;
+int lastButtonState;
+int relayState;
+int previousRelayState;
+
+char cmdTopic[15];
+char opstaTopic[17];
+char mqttClientId[17];
+
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+
+String getConf(String type){
+  String retVal;
+  String chipId = String(ESP.getChipId()); // 32bit unsigned int (max value 4294967296, 10 digits)
+
+  if(type == "chipID"){
+    retVal = chipId;
+  }
+  if(type == "clientID"){
+    retVal = "S20ESP" + chipId;
+  }
+  if(type == "TC"){ // Topic Command
+    retVal = chipId + "/cmd";
+  }
+  if(type == "TS"){ // Topic State
+    retVal = chipId + "/opsta";
+  }
+  
+  return retVal;
+}
+
+int connectAttemptsMqtt = 0;
+long lastConnectAttemptMqtt = 0;
+
+bool connectMqtt() {
+
+  bool retVal = false;
+  
+  client.setServer(mqttServer, mqttServerPort);
+  client.setCallback(mqttCallback);
+
+  getConf("clientID").toCharArray(mqttClientId, 15);
+  getConf("TC").toCharArray(cmdTopic, 15);
+  
+  if(client.connected()){
+
+    #ifdef DEBUG
+      Serial.println("MQTT Already connected.");
+    #endif
+    
+    retVal = true;
+    return retVal;
+    
+  }
+
+  connectAttemptsMqtt = millis() - lastConnectAttemptMqtt > MQTT_CONN_TIMEOUT ? 0 : connectAttemptsMqtt;
+
+  #ifdef DEBUG
+    if(connectAttemptsMqtt == MQTT_MAX_ATTEMPT){
+      Serial.println("Max reconnects reached, trying again later.");
+      Serial.println(millis());
+    }
+  #endif
+
+  if(millis() - lastConnectAttemptMqtt > MQTT_CONN_REATT && connectAttemptsMqtt < MQTT_MAX_ATTEMPT){
+
+    #ifdef DEBUG
+      Serial.print("MQTT Client ID: ");
+      Serial.println(mqttClientId);
+      Serial.print("Connecting to MQTT broker: ");
+      Serial.print(mqttServer);
+      Serial.print(":");
+      Serial.println(mqttServerPort);
+    #endif
+
+    if (client.connect(mqttClientId, mqttUsername, mqttPassword)) {
+      #ifdef DEBUG
+        Serial.println("Connected.");
+      #endif
+      if (client.subscribe(cmdTopic)) {
+        #ifdef DEBUG
+          Serial.print("Listening to ");
+          Serial.println(cmdTopic);
+        #endif
+      } else {
+        #ifdef DEBUG
+          Serial.print("failed, rc=");
+          Serial.print(client.state());
+          Serial.println(" try again in 5 seconds");
+        #endif
+        
+        retVal = false;
+        return retVal;
+        
+      }
+    } else {
+      #ifdef DEBUG
+        Serial.println("Connection failed, trying again.");
+        Serial.println(millis());
+      #endif
+    }
+    
+    connectAttemptsMqtt++;
+    lastConnectAttemptMqtt = millis();
+
+  }
+  
+  retVal = true;
+  return retVal;
+  
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  char c_payload[length];
+  memcpy(c_payload, payload, length);
+  c_payload[length] = '\0';
+
+  String s_topic = String(topic);
+  String s_payload = String(c_payload);
+
+  if (s_topic == getConf("TC")) {
+    if (s_payload == "ON") {
+      relayState = HIGH;
+    } else if (s_payload == "OFF") {
+      relayState = LOW;
+    }
+  }
+}
+
+void setup() {
+  delay(2000);
+  
+  #ifdef DEBUG
+    Serial.begin(9600);
+    delay(2000);
+    Serial.print("Connecting to WiFi: ");
+    Serial.println(wifiSsid);
+    Serial.print("Using password: ");
+    Serial.println(wifiPass);
+  #endif
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifiSsid, wifiPass);
+  delay(10000);
+
+  #ifdef DEBUG
+    Serial.println(".");
+    if(WiFi.status() != WL_CONNECTED){
+      Serial.println("WiFi connection failed.");
+    } else {
+      Serial.println("WiFi connected");
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP());
+    }
+  #endif
+
+  if(WiFi.status() == WL_CONNECTED){
+    
+    if(connectMqtt()){
+      getConf("chipID").toCharArray(mqttClientId, 11);
+      client.publish(mqttClientId, FW_VERSION);
+      #ifdef DEBUG
+        Serial.println("MQTT Connected.");
+        Serial.print("MQTT-Client ID: ");
+        Serial.println(getConf("clientID"));
+        Serial.print("Command topic: ");
+        Serial.println(getConf("TC"));
+        Serial.print("State topic: ");
+        Serial.println(getConf("TS"));
+      #endif
+    } else {
+      #ifdef DEBUG
+        Serial.println("MQTT Connection failed.");
+      #endif
+    }
+  
+  } else {
+    
+    #ifdef DEBUG
+      Serial.println("WiFi not connected, skipping MQTT.");
+    #endif
+    
+  }
+  
+  EEPROM.begin(4);
+  relayState = EEPROM.read(0) == 0 ? LOW : HIGH;
+  previousRelayState = relayState;
+
+  #ifdef DEBUG
+    Serial.print("Setting relay output: ");
+    Serial.println(relayState);
+  #endif
+  
+  pinMode(PIN_BUTTON, INPUT);
+  pinMode(PIN_STATUS, OUTPUT);
+  pinMode(PIN_RELAY, OUTPUT);
+  digitalWrite(PIN_STATUS, HIGH);
+  digitalWrite(PIN_RELAY, relayState);
+
+  #ifdef DEBUG
+    Serial.println("Outputs set.");
+  #endif
+}
+
+
+void loop() {
+  long now = millis();
+
+  client.loop();
+
+  if(!client.connected()){
+    digitalWrite(PIN_STATUS, LOW); // Light green LED if WiFi or MQTT not connected.
+    connectMqtt();
+  } else {
+    digitalWrite(PIN_STATUS, HIGH);
+  }
+  
+  int buttonReading = digitalRead(PIN_BUTTON);
+  if (buttonReading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((now - lastDebounceTime) > debounceDelay) {
+    if (buttonReading != buttonState) {
+      buttonState = buttonReading;
+      if (buttonState == LOW) { // When button is pressed, input is low
+        relayState = !relayState;
+        
+        #ifdef DEBUG
+          Serial.println("Button pressed.");
+        #endif
+      }
+    }
+  }
+
+  lastButtonState = buttonReading;
+
+  if (lastDebounceTime > millis()) {
+    lastDebounceTime = 0;
+  }
+
+  if (relayState != previousRelayState) {
+    #ifdef DEBUG
+      Serial.println("Toggling relay.");
+    #endif
+    digitalWrite(PIN_RELAY, relayState);
+    previousRelayState = relayState;
+    reportRequired = true;
+  }
+
+  if (reportRequired) {
+    #ifdef DEBUG
+      Serial.println("Reporting relay state.");
+    #endif
+    
+    getConf("TS").toCharArray(opstaTopic, 17);
+    
+    if (relayState == HIGH) {
+      client.publish(opstaTopic, "ON");
+    } else {
+      client.publish(opstaTopic, "OFF");
+    }
+    EEPROM.write(0, relayState);
+    EEPROM.commit();
+    reportRequired = false;
+  }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# ESP8266_R1CH
+# ITEAD-S20-CustomFW
 An Arduino sketch used to control a relay output, communicating both ways via MQTT.
+
+## What is it
+A sketch written for ITEAD S20 Smart Socket, built upon ESP8266, and allows the user to control the device via the local network independent from the manufacturers servers.
+
+It can be easily ported to other platforms/devices by altering the includes, pin-assignment and the chip id determination.
+
+## Features
+
+* Turn on or off the relay output by sending a MQTT-message via local MQTT-broker.
+* Toggle the relay using the local button.
+
+## How it works
+
+The device subscribes to the command MQTT-topic of the following syntax; `chipID/cmd`. When a command is received, the device switches the relay on or off depending on the message and confirms it's new state to the status topic of the following syntax; `chipID/opsta`.
+
+`chipID` is read from the ESP8266, this ensures it will be unique in your network.
+
+Switch on by posting the message `ON` to the command-topic, switch off by posting the message `OFF`. Observe that this message is case-sensitive.
+
+When reporting to the status-topic, the device will send `ON` if the relay output is switched on or `OFF` if it is switched off.
+
+The relay can also be toggled via the local button. When the relay is toggled it will report it's new state to the status topic immediately.
+
+The device continously saves the current relay state to the EEPROM and automatically restore it's state and reports it to the broker after power loss.
+
+At boot the device will send it's firmware version (`FW_VERSION`, below) to the topic named as it's MQTT Client ID, below. This functionality is provided to assist users in determining the currently installed firmware and in the future, if upgrade is needed.
+
+## Configuration
+
+### Defines 
+
+| Parameter | Description | Default value |
+| --- | --- | --- |
+| FW_VERSION | Version reported by the device when successfully connected to the MQTT-broker | Release dependent |
+| PIN_RELAY | Specifies the GPIO-pin used to control the relay output. | 12 (GPIO12) |
+| PIN_STATUS | Specifies the GPIO-pin used to control the status led. | 13 (GPIO13) |
+| PIN_BUTTON | Specifies the GPIO-pin where the local button is connected. | 0 (GPIO0) |
+| MQTT_MAX_ATTEMPT | Specifies how many times the device will try to connect to the MQTT-broker before waiting for `MQTT_CONN_TIMEOUT` time to pass before retrying | 3 |
+| MQTT_CONN_TIMEOUT | Specifies the timeout after `MQTT_MAX_ATTEMPT` failed connection attempts. | 60000 (60s) |
+| MQTT_CONN_REATT | Specifies the timeout between two connection attempts. | 10000 (10s) |
+
+### Variables
+
+| Parameter | Description | Default value |
+| --- | --- | --- |
+| wifiSsid | The name of the network. (SSID). | N/A |
+| wifiPass | The password to use when connection to the network `wifiSsid`. | N/A |
+| mqttServer | The IP-address of the MQTT-broker. | N/A |
+| mqttServerPort | The port where the MQTT-broker is listening. | 1883 |
+| mqttUsername | Username used to authenticate to the MQTT-broker. | N/A |
+| mqttPassword | Password used to authenticate to the MQTT-broker. | N/A |
+
+## Examples
+Example of generated topics and id's if the device has the ID 1327465.
+
+| Parameter | Resulting value | Dedeviceion |
+| --- | --- | --- |
+| MQTT Client ID | S20ESP1327465 | ID used in communication with MQTT-broker |
+| Command topic | 1327465/cmd | Topic where the device will listen for commands `ON` or `OFF` to set the relay output. |
+| Status topic | 1327465/opsta | Topic where the device will report back it's current state. |
+
+### OpenHAB
+Example of item specification in OpenHAB.
+
+```
+Switch lmp "Window" <light> (lights) {mqtt=">[MQTTBR01:1327465/cmd:command:ON:default], >[MQTTBR01:1327465/cmd:command:OFF:default], <[MQTTBR01:1327465/opsta:state:default]"}
+```
